628aa9fd3a7125b978160216b74b771e2d4e0561
m4n1c22 <sreeram.sadasivam@gmail.com>
2015-10-28 21:59:43 +0100

    Makefile modified. Comments added to task1.1...

--- os_lab_1/task1_1.c
+++ os_lab_1/task1_1.c
@@ -11,33 +11,60 @@
 
 //Global variable generated for the processes.
 int my_value = 42;
-
+/**
+		\brief:	Function which implements the task 1.1.
+				It discusses the implementation of fork and wait system
+				calls.
+*/
 void task_1_1_function() {
 
 	pid_t return_fork;
 	pid_t return_wait;
+
 	return_fork = fork();
-	usleep(150000);
+	
+	// Error handling for function fork().
 	if(return_fork<0) {
 		
 		fprintf(stderr, "%s\n","Error in execution of fork function.");
 		exit(-1);
 	
-	}	else if(return_fork==0) {
+	}	
+	/*
+		Condition block run by the child process since, fork function returns 0 
+		as the return value for function fork().
+	*/
+	else if(return_fork==0) {
 
+		//The process sleeps for 150ms.
+		usleep(150000);
+		
+		//my_value is modified by the child process from 42 to a different value.
 		my_value = 18951;
 		fprintf(stderr, "%s%d\n", "Child Process Execution. My Value ",my_value);
+		fprintf(stderr, "%s%d\n", "From Child -> PID: ",getpid());
+
+		//The child process sleeps for 500ms.
 		usleep(500000);
-	}	else {
-
-
+	}
+	/*
+		Condition block run by the parent process since, fork function returns pid
+		of the child process as the  return value for function fork().
+	*/
+	else {
+		
+		//The process sleeps for 150ms.
+		usleep(150000);
 		fprintf(stderr, "%s%d\n", "Parent Process Execution. My Value ",my_value);
 		return_wait = wait(NULL);
+
+		//Error handling for the wait() function call.
 		if(return_wait == -1) {
 
 			fprintf(stderr, "%s\n","Error in execution of wait function.");
 			exit(-1);		
 		}
+
 		fprintf(stderr, "%s%d\n", "Parent PID: ",getpid());
 		fprintf(stderr, "%s%d\n", "Child PID: ",return_fork);
 	}
