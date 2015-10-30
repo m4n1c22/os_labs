7467a6fd3cb27bfd40138f9fd9b48a3195b9e571
Sreeram Sadasivam <sreeram.sadasivam@gmail.com>
2015-10-30 23:56:45 +0100

    minor bug fixes

--- os_lab_1/task2_1.c
+++ os_lab_1/task2_1.c
@@ -19,9 +19,9 @@ void *task_2_1_function() {
 
 	usleep(150000);
 	my_value = 18951;
-	fprintf(stderr, "%s%d\n", "Child Process Execution. My Value ",my_value);
+	fprintf(stderr, "%s%d\n", "Child Thread Execution. My Value ",my_value);
 
-	//The child process sleeps for 500ms.
+	//The child thread sleeps for 500ms.
 	usleep(500000);
 	pthread_exit(0);
 }
@@ -31,19 +31,22 @@ int main() {
 
 	int err;
 
-	pthread_t custom_threads;
+	pthread_t custom_thread;
 
 	/** Thread creation */
-	err = pthread_create(&custom_threads,NULL,task_2_1_function,NULL);
+	err = pthread_create(&custom_thread,NULL,task_2_1_function,NULL);
 		
 	if(err!=0) {
 
 		exit(EXIT_FAILURE);
 	}
 	usleep(150000);
-	fprintf(stderr, "%s%d\n", "Parent Process Execution. My Value ",my_value);
+	fprintf(stderr, "%s%d\n", "Parent Thread Execution. My Value ",my_value);
 	err = pthread_join(custom_threads,NULL);
+	if(err!=0) {
 
+		exit(EXIT_FAILURE);
+	}
 
 	return 0;
 }
