4fc4639db7df3ef81e06ee592e9640375f7194c0
ad0vith <advith.belegaddenagappa@stud.tu-darmstadt.de>
2015-11-09 15:49:29 +0100

    comments added in task3

--- os_lab_1/task3_1.c
+++ os_lab_1/task3_1.c
@@ -18,7 +18,7 @@ void task_3_1_function() {
         
 	
 
-	
+	/*open a pipe and check for errors in pipe creation*/
 
 	if(pipe(fd)==-1){
 	fprintf(stderr,"Pipe creation failed\n");	
@@ -27,9 +27,10 @@ void task_3_1_function() {
 	fprintf(stderr,"pipe creation successful\n");
 	}
 
-	
+	/*fork a child process*/
 	return_fork = fork();
-
+	
+	/*check for errors in child process creation*/
 	if(return_fork<0) {
 		
 		fprintf(stderr, "%s\n","Error in execution of fork function.");
@@ -37,31 +38,37 @@ void task_3_1_function() {
 	
 	}	
 
-	
+	/*Condition block run by the parent since fork function returns the PID of the child to the parent process*/
 	else if(return_fork > 0){
-		
+		/*sleep for 150 ms*/
 		usleep(150000);
+
 		fprintf(stderr, "%s%d\n", "Parent Process Execution. My Value\n ",my_value);
-		
+
+		/*create the message to be passed on to the child process*/
 		sprintf(buffer, "Hi, I am your parent. My PID=%d and my_value=%d", getpid(), my_value);
 
+		/*since the Parent proceess only sends a message to its child, close the read channel of the pipe*/
         	close(fd[0]);
 
-        	
+        	/*write the message into the Write channel of the pipe*/
 		write(fd[1],buffer,sizeof(buffer));	
 		
-        	
+        	/*wait for the child process to finish execution*/
 		return_wait = wait(NULL);
 
-		
+		/*check for errors in execution of the child process*/
 		if(return_wait == -1) {
 
 			fprintf(stderr, "%s\n","Error in execution of wait function.");
 			exit(EXIT_FAILURE);		
 		}
-
+		
+		/*print the PID of the Parent and the Child*/
 		fprintf(stderr, "%s%d\n", "Parent PID: ",getpid());
 		fprintf(stderr, "%s%d\n", "Child PID: ",return_fork);
+
+		/*close the write channel of the pipe*/
 		close(fd[1]);
 		exit(EXIT_SUCCESS);
 
@@ -73,19 +80,22 @@ void task_3_1_function() {
 	*/
 	else {
 
-		
+		/*sleep for 150 ms*/		
 		usleep(150000);
 		
-		
+		/*since child only reads the message from parent, close the write channel of the pipe*/
 		close(fd[1]);
 		
-
+		/*read the content of the read channel of the pipe*/
 		read(fd[0],buffer,sizeof(buffer));
 
+		/*sleep for 500 ms*/
 		usleep(500000);
+
+		/*print the received message*/
 		fprintf(stderr,"Received message at child is %s\n",buffer);
 
-	
+		/*close the read channel of the pipe*/
 		close(fd[0]);
 		exit(EXIT_SUCCESS);
 	}
